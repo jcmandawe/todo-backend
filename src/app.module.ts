9b650bc2f1999368571d00e459d594382fce4aeb
John Carlo Mandawe <jcmandawe@finalehealth.com>
2024-04-24 19:57:28 +0800

    FEAT: install mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: 'mongodb+srv://root:root@todo.fkn1v3q.mongodb.net/?retryWrites=true&w=majority&appName=todo',
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
